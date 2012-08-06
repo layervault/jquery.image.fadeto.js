4d8223f3a03012152520fca65759cfe4f619c73c
Kelly Sutton <michael.k.sutton@gmail.com>
2012-08-06 12:24:59 -0400

    fading correctly and cleaning up after ourselves

--- src/jquery.image.fadeto.js
+++ src/jquery.image.fadeto.js
@@ -9,14 +9,16 @@
     hasSetup,
     options,
     prefixes,
-    randomClass;
+    randomClass,
+    positionNewImage,
+    positionClassesFrom;
 
   options = {
-    fadeInLength: 250,
-    fadeOutLength: 250,
-    callback: null,
-    fadeOutEasing: 'ease-in',
-    fadeInEasing:  'ease-out'
+    fadeLength      : 1000,
+    callback        : null,
+    fadeOutEasing   : 'ease-in-out',
+    fadeInEasing    :  'ease-in-out',
+    hijackClasses   : true
   };
 
   prefixes = [
@@ -37,30 +39,82 @@
       fadeInTransition,
       fadeOutTransition,
       fadeInClass = randomClass(),
-      fadeOutClass = randomClass();
+      fadeOutClass = randomClass(),
+      zeroOpacityClass = randomClass();
 
     options = $.extend(options, opts);
 
     fadeInTransition = $.map(prefixes, function (p) {
-      return p + "transition: opacity " + options.fadeInLength + "ms " + options.fadeInEasing + ";"
+      return p + "transition: opacity " + options.fadeLength + "ms " + options.fadeLength
     }).join(' ');
 
     fadeOutTransition = $.map(prefixes, function (p) {
-      return p + "transition: visibility 0s " + options.fadeOutLength + "ms, opacity " + options.fadeInLength + "ms " + options.fadeInEasing + ";"
+      return p + "transition: visibility 0s " + options.fadeLength + "ms, opacity " + options.fadeLength + "ms " + options.fadeInEasing + ";"
     }).join(' ');
 
     $style.html(
-      "." + fadeInClass  + " { visibility: visible; opacity: 1; " + fadeInTransition  + " } " +
-      "." + fadeOutClass + " { visibility: hidden;  opacity: 0; " + fadeOutTransition + " }"
+      "." + fadeInClass       + " { visibility: visible; opacity: 1; " + fadeInTransition  + " } " +
+      "." + fadeOutClass      + " { visibility: hidden;  opacity: 0; " + fadeOutTransition + " }" +
+      "." + zeroOpacityClass  + " { visibility: visible; opacity: 0; }"
     );
 
     $head.append($style);
 
     return {
-      $style  : $style,
-      fadeIn  : fadeInClass,
-      fadeOut : fadeOutClass
+      $style      : $style,
+      fadeIn      : fadeInClass,
+      fadeOut     : fadeOutClass,
+      zeroOpacity : zeroOpacityClass
     };
+  };
+
+  /*
+    Create a throwaway class to aboslutely posiiton an image.
+  */
+  positionClassesFrom = function ($oldImage) {
+    var
+      transitionImagePositionClass = randomClass(),
+      oldImagePositionClass = randomClass(),
+      $head  = $('head'),
+      $style = $('<style></style>'),
+      top,
+      left,
+      zIndex = parseInt($oldImage.attr('z-index'), 10);
+
+    if (typeof $oldImage === "undefined" || $oldImage.length === 0) {
+      return null;
+    }
+
+    if (isNaN(zIndex)) {
+      zIndex = 1;
+    }
+
+    top = $oldImage.position().top;
+    left = $oldImage.position().left;
+
+    $style.html(
+      "." + transitionImagePositionClass + " { position: absolute; top: " + top + "px; left: " + left + "px; z-index: " + zIndex +"; }" +
+      "." + oldImagePositionClass + "{ position: absolute; top: " + top + "px; left: " + left + "px; z-index: " + (zIndex + 1) + "; }"
+    );
+
+    $head.append($style);
+
+    return {
+      oldImage        : oldImagePositionClass,
+      transitionImage : transitionImagePositionClass,
+      $style          : $style
+    }
+  };
+
+  positionNewImage = function ($newImage, $oldImage) {
+    var classes = positionClassesFrom($oldImage)
+
+    $newImage.data('positionClass', classes.transitionImage);
+    $newImage.addClass($newImage.data('positionClass'));
+
+    $oldImage.addClass(classes.oldImage);
+
+    return classes;
   };
 
   //-- Methods to attach to jQuery sets
@@ -69,7 +123,11 @@
     var
       $e = $(this),
       classes,
-      newImage;
+      transitionClasses,
+      $transitionImage = $('<img />'),
+      oldClasses = [],
+      $newImage = $('<img />'),
+      $imageParent;
 
     if (typeof opts === "function") {
       opts = {
@@ -80,30 +138,49 @@
       opts.callback = callback
     }
 
+    if ($e.length === 0) {
+      return;
+    }
+
     // We're not dealing with an image tag. Bail out.
-    if ($e.attr('tagName').toLowerCase() !== "img") {
+    if ($e.get(0).tagName.toLowerCase() !== "img") {
       return;
     }
 
     classes = setup(opts);
+    transitionClasses = positionNewImage($transitionImage, $e);
+
+    $('body').append($transitionImage);
 
     newImage = new Image();
 
     newImage.onload = function () {
-      $e.addClass(classes.fadeOut);
-      window.setTimeout(function () {
-        $e.attr('src', newSource);
-        $e.addClass(classes.fadeIn);
-        $e.removeClass(classes.fadeOut);
-        window.setTimeout(function () {
-          $e.removeClass(classes.fadeIn);
-          classes.$style.remove();
+      $transitionImage.attr('src', newSource);
+      $newImage.attr('src', newSource);
 
-          if (typeof options.callback === "function") {
-            options.callback();
-          }
-        }, options.fadeInLength);
-      }, options.fadeOutLength);
+      $e.addClass(classes.fadeOut);
+
+      // Once the fades are done, remove the original image and shuffle new image into the correct
+      // place on the DOM.
+      window.setTimeout(function () {
+        $imageParent = $e.parent();
+
+        if (options.hijackClasses) {
+          $.each($e.get(0).classList, function (index, item) {
+            $newImage.addClass(item);
+          });
+        }
+
+        $e.remove();
+        $imageParent.append($newImage);
+        classes.$style.remove();
+        transitionClasses.$style.remove();
+        $transitionImage.remove();
+
+        if (typeof options.callback === "function") {
+          options.callback();
+        }
+      }, options.fadeLength);
       newImage.onload = null;
     };
 
